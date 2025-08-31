a03461501d42251f3f8b120ecce5ca8b5b88b3f7
arcbjorn <arky.happ@gmail.com>
2025-08-31 01:04:34 -0300

    feat: add import aliases to SvelteKit configuration

--- app/svelte.config.js
+++ app/svelte.config.js
@@ -6,7 +6,21 @@ const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: { 
+		adapter: adapter(),
+		alias: {
+			'@': 'src',
+			'@components': 'src/lib/components',
+			'@services': 'src/lib/services',
+			'@stores': 'src/lib/stores',
+			'@config': 'src/lib/config',
+			'@utils': 'src/lib/utils',
+			'@types': 'src/lib/types',
+			'@contracts': 'src/lib/contracts',
+			'@routes': 'src/routes',
+			'@packages': '../packages'
+		}
+	}
 };
 
 export default config;
